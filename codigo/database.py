286d1d5144562419ae682929c1170c4dbb1a5d8c
kauerico <ribeirokaue70@gmail.com>
2024-07-19 22:19:31 -0300

    Ocultando credenciais

--- codigo/database.py
+++ codigo/database.py
@@ -1,9 +1,20 @@
+from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = f"mysql+pymysql://kaue:123@db/database_app"
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Obtém as credenciais do MySQL das variáveis de ambiente
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_DB = os.getenv('MYSQL_DB')
+
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
