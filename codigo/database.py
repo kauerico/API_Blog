ab74c80a7b8123dec9c6add521855f732544fa55
kauerico <ribeirokaue70@gmail.com>
2024-07-30 16:47:55 -0300

    Agora vai

--- codigo/database.py
+++ codigo/database.py
@@ -1,20 +1,8 @@
-from dotenv import load_dotenv
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
-# Carrega as variáveis de ambiente do arquivo .env
-load_dotenv()
-
-# Obtém as credenciais do MySQL das variáveis de ambiente
-MYSQL_USER = os.getenv('MYSQL_USER')
-MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-MYSQL_HOST = os.getenv('MYSQL_HOST')
-MYSQL_DB = os.getenv('MYSQL_DB')
-
-DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+DATABASE_URL = f"mysql+pymysql://kaue:123@db:3306/blog_data"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
