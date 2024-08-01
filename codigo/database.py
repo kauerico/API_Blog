3d493106a1a566f5fd1c7a8354edf955811bb280
kauerico <ribeirokaue70@gmail.com>
2024-08-01 08:43:26 -0300

    Resolvido

--- codigo/database.py
+++ codigo/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = f"mysql+pymysql://kaue:123@db:3306/blog_data"
+DATABASE_URL = f"mysql+pymysql://kaue:123@db:3308/blog_data"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
